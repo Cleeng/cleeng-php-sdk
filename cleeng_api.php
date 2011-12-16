b2dcf4c0e4d47472d71622981cbd8b95ed04a545
Mateusz Tymek <mtymek@gmail.com>
2011-12-16 15:17:57 +0100

    Cleanup

--- cleeng_api.php
+++ cleeng_api.php
@@ -2,19 +2,6 @@
 /**
  * Load all Cleeng classes 
  */
-foreach (return array(
-    'Cleeng_AbstractTransport' => dirname(__FILE__) . '/AbstractTransport.php',
-    'Cleeng_Api' => dirname(__FILE__) . '/Api.php',
-    'Cleeng_Transport_Curl' => dirname(__FILE__) . '/Transport/Curl.php',
-    'Cleeng_Transfer_Object' => dirname(__FILE__) . '/TransferObject.php',
-)
-
-include dirname(__FILE__) . '/Cleeng/autoload_classmap.php' as $file)
-{
-    require_once $file;
-} 
-
-
 foreach (array(
 	    'AbstractTransport.php',
 	    'Transport/Curl.php',

55324e9ecd970ba6192b081b0dafbb7895e57d10
tomekszad <tomek.szad@gmail.com>
2012-11-19 15:12:37 +0100

    array of files to include

--- cleeng_api.php
+++ cleeng_api.php
@@ -6,15 +6,34 @@
  * autoloader (like WordPress plugin)
  */
 foreach (array(
-            'Api.php',            
-            'InvalidArgumentException.php',
-            'RuntimeException.php',
-            'Transport/Abstract.php',
+            'Api.php',
+            'Entity/Base.php',
+            'Entity/Customer.php',
+            'Entity/SingleOffer.php',
+            'Entity/RemoteAuth.php',
+            'Entity/CustomerToken.php',
+            'Entity/Associate.php',
+            'Entity/Collection.php',
+            'Entity/CustomerRental.php',
+            'Entity/RentalOffer.php',
+            'Entity/Publisher.php',
+            'Entity/CustomerSubscription.php',
+            'Entity/OperationStatus.php',
+            'Entity/SubscriptionOffer.php',
+            'Entity/AccessStatus.php',
+            'Exception/ExceptionInterface.php',
+            'Exception/RuntimeException.php',
+            'Exception/InvalidArgumentException.php',
+            'Exception/HttpErrorException.php',
+            'Exception/ApiErrorException.php',
+            'Exception/InvalidJsonException.php',
+            'Transport/AbstractTransport.php',
             'Transport/Curl.php',
-            'TransferObject.php',
+
                 ) as $_cleeng_class_file) {
 
     require_once dirname(__FILE__) . '/src/Cleeng/' . $_cleeng_class_file;
+
 }
 
 unset($_cleeng_class_file);
